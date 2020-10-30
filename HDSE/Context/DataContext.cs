e0410c858b828253bb3e5607bdac3d48e0df2614
Sneak Quang <quangbae30@gmail.com>
2020-10-30 16:56:12 +0700

    bac day nho

---
+++ HDSE/Context/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using HDSE.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HDSE.Context
+{
+    public class DataContext :DbContext
+    {
+        public DataContext() : base("T1907AEntities")
+        {
+
+        }
+        public  DbSet<Employee> employees { get; set; }
+       
+    }
+    
+}
---
+++ HDSE/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HDSE.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Required(ErrorMessage = "ID khong duoc duoc trong")]
+        public string EmployeeId { get; set; }
+        [Required(ErrorMessage ="Ten khong duoc duoc trong")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Phong ban khong duoc de trong")]
+        public string Department { get; set; }
+        [Required(ErrorMessage ="Khong duoc de trong tien")]
+        public double Salary { get; set; }
+    }
+}
